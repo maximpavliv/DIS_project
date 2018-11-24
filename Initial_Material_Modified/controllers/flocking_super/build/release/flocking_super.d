de261e4a8a06a98b1e58648cadfa825b86145069
maximpavliv <maxim.pavliv@epfl.ch>
2018-11-24 21:02:09 +0100

    reynolds 3rd rule removed, the reynolds swarming seems to work well. Braitenberg doednt work

---
+++ Initial_Material_Modified/controllers/flocking_super/build/release/flocking_super.d
@@ -0,0 +1,6 @@
+build/release/flocking_super.o: flocking_super.c \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/robot.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/types.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/nodes.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/emitter.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/supervisor.h
---
+++ Initial_Material_Modified/controllers/reynolds/build/release/reynolds.d
@@ -0,0 +1,9 @@
+build/release/reynolds.o: reynolds.c \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/robot.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/types.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/nodes.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/motor.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/differential_wheels.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/distance_sensor.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/emitter.h \
+ /home/maxim/webots_2018b_amd64/usr/local/webots/include/controller/c/webots/receiver.h
